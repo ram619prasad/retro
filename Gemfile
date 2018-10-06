ba97ab860fe47fe443c2098b4cd9df3f1fadca93
“rpr_maddi” <ram.prasad@introhive.com>
2018-10-07 00:03:09 +0530

    Retro: Test suite setup
    * Included all the gems needed for running the tests.
    
    * TODO: Still need to add the tests though :p

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'rack-attack'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'parallel_tests'
+  gem 'factory_bot_rails'
 end
 
 group :development do
@@ -44,6 +46,16 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+# Testing Suite Gems
+group :test do
+  gem 'test-unit'
+  gem 'faker'
+  gem 'database_cleaner'
+  gem 'shoulda'
+  gem 'shoulda-matchers', '4.0.0.rc1'
+  gem 'simplecov', require: false
 end
 
 # Authentication 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,32 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'parallel_tests/test/runtime_logger' if ENV['RECORD_RUNTIME']
+require 'database_cleaner'
+require 'simplecov'
+
+
+SimpleCov.start 'rails' do
+  add_filter "app/mailers/application_mailer.rb"
+  add_filter "app/models/application_record.rb"
+  add_filter "app/jobs/application_job.rb"
+end
+
+DatabaseCleaner.strategy = :truncation
+
+# then, whenever you need to clean the DB
+# DatabaseCleaner.clean
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def setup
+    DatabaseCleaner.start
+  end
+
+  def teardown
+    DatabaseCleaner.clean
+  end
 end
